d35aaf4a5f9f0e7344caa68ffdd835d4b4bb64a3
devgl96 <georgelucas.dev@gmail.com>
2021-02-26 14:28:35 -0300

    Project Complete

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const {level} = useContext(ChallengesContext);
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/devgl96.png" alt="George Lucas" />
+            <div>
+                <strong>George Lucas</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
